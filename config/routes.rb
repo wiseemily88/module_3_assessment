d8dbb432321ad755240293beac7973fcb513f59d
Emily Wise <wise.m.emily@gmail.com>
2018-01-02 09:31:07 -0700

    Add Item Show endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :items, only: [:index]
+      resources :items, only: [:index, :show]
     end
-  end    
+  end
 
 end
--- spec/api/v1/items_request_spec.rb
+++ spec/api/v1/items_request_spec.rb
@@ -10,8 +10,19 @@ describe 'Items Api' do
   items = JSON.parse(response.body)
 
   expect(items.count).to eq(4)
-  
   end
+
+  it 'can return one item with a matching id' do
+    id = create(:item).id
+
+    get "/api/v1/items/#{id}"
+
+    expect(response).to be_success
+
+     item = JSON.parse(response.body)
+
+     expect(item["id"]).to eq(id)
+   end 
 
 
 
